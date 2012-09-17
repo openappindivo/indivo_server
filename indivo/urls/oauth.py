361829872cb8d9e6b067f8a16ee6c1ee4feedddb
adamrbates <adam.r.bates@gmail.com>
2012-09-17 14:37:39 -0400

    CHANGE: switched out depricated django.conf.urls.defaults with django.conf.urls

--- indivo/urls/oauth.py
+++ indivo/urls/oauth.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 #from oauth.djangoutils import request_token, exchange_token, PARAMS
 #PARAMS['OAUTH_SERVER'] = OAUTH_SERVER
--- indivo/urls/record_carenets.py
+++ indivo/urls/record_carenets.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import *
 
 from indivo.views import *
 from indivo.lib.utils import MethodDispatcher
